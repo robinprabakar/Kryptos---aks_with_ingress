6b80ddb818480e4e52208ac0138113109a964501
robinprabakar <107672035+robinprabakar@users.noreply.github.com>
2022-09-02 11:26:15 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "node_count" {
+    default = 1
+}
+
+variable "dns_prefix" {
+    default = "k8stest"
+}
+
+variable "cluster_name" {
+    default = "k8stest"
+}
+
+variable "resource_group_name" {
+    default = "azure-k8stest"
+}
+
+variable "location" {
+    default = "West Europe"
+#    default = "Australia East" # available on free tier
+}
+
+variable "vm_size" {
+    default = "Standard_B2s"
+}
